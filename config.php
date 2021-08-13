b7904eb760c4af63f2241554ac9f0452ea07a1e2
Tohir <hakimovtohirbek@gmail.com>
2021-08-13 11:26:29 +0500

    first commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'debug' => true,
+  'database' =>
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'forum_oyina',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+    'engine' => NULL,
+    'prefix_indexes' => true,
+  ),
+  'url' => 'http://forum.oyina.uz',
+  'paths' =>
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+  'headers' =>
+  array (
+    'poweredByHeader' => true,
+    'referrerPolicy' => 'same-origin',
+  ),
+);
